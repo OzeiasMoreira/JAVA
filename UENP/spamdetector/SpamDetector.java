375d543e8f71a0c11366642bacf8e870f696b664
= <=>
2024-05-21 00:38:22 -0300

    comentando o codigo

--- UENP/spamdetector/SpamDetector.java
+++ UENP/spamdetector/SpamDetector.java
@@ -18,9 +18,9 @@ public class SpamDetector {
     public static String calcularScore(String mensagem) {
         Integer score = 0;
 
-        for (String[] termo : termos) { //p cada termo dentro de termos
-            String palavraChave = termo[0];
-            Integer peso = Integer.parseInt(termo[1]);
+        for (String[] termo : termos) { //p cada termo dentro de termos,executa o que esta dentro desse for
+            String palavraChave = termo[0]; // Recebe a coluna 0,ex Gratis
+            Integer peso = Integer.parseInt(termo[1]); // passa a coluna 1 para Inteiro
             
             //sera que na minha mensagem contem a palavra chave "Gratis"?
             if (mensagem.toLowerCase().contains(palavraChave.toLowerCase())) {
